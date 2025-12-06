e7a6028d5dad243306b1a48d786786bc0ed85883
ThatsJustDoran <thatsjustdoran@gmail.com>
2025-12-06 19:07:44 +0400

    day 1 solution

--- 2025/01/code.go
+++ 2025/01/code.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"strconv"
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
@@ -15,10 +18,35 @@ func main() {
 // 4. with: true (part2), and user input
 // the return value of each run is printed to stdout
 func run(part2 bool, input string) any {
+	inputSlc := strings.Split(input, "\n")
+	resOver0 := 0
+	res := 0
+	curV := 50
+	for _, act := range inputSlc {
+		n, _ := strconv.Atoi(act[1:])
+		if n > 100 {
+			resOver0 += n / 100
+			n = n % 100
+		}
+
+		if act[0] == 'L' {
+			if curV != 0 && curV-n < 0 {
+				resOver0++
+			}
+			curV = (curV + 100 - n) % 100
+		} else {
+			if curV+n > 100 {
+				resOver0++
+			}
+			curV = (curV + n) % 100
+		}
+		if curV == 0 {
+			res++
+		}
+	}
 	// when you're ready to do part 2, remove this "not implemented" block
 	if part2 {
-		return "not implemented"
+		return resOver0 + res
 	}
-	// solve part 1 here
-	return 42
+	return res
 }
