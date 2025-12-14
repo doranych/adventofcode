6da871c1f4c81977a887bf344075cbc8afc8c108
ThatsJustDoran <thatsjustdoran@gmail.com>
2025-12-14 02:02:40 +0200

    day 2 solution

--- 2025/02/code.go
+++ 2025/02/code.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/jpillora/puzzler/harness/aoc"
 )
 
@@ -15,10 +19,39 @@ func main() {
 // 4. with: true (part2), and user input
 // the return value of each run is printed to stdout
 func run(part2 bool, input string) any {
-	// when you're ready to do part 2, remove this "not implemented" block
-	if part2 {
-		return "not implemented"
+	intervals := strings.Split(input, ",")
+	res := 0
+	for _, r := range intervals {
+		rng := strings.Split(r, "-")
+		low, _ := strconv.Atoi(rng[0])
+		high, _ := strconv.Atoi(rng[1])
+		for i := low; i <= high; i++ {
+			val := fmt.Sprintf("%d", i)
+			if !part2 {
+				if val[0:len(val)/2] == val[len(val)/2:] {
+					res += i
+				}
+				continue
+			}
+
+			for j := 0; j <= len(val)/2; j++ {
+				p := val[0 : j+1]
+				if len(val)%len(p) != 0 {
+					continue
+				}
+				count := 1
+				for c := len(p); c < len(val); c += len(p) {
+					if val[c:c+len(p)] != p {
+						break
+					}
+					count++
+				}
+				if count != 1 && count == len(val)/len(p) {
+					res += i
+					break
+				}
+			}
+		}
 	}
-	// solve part 1 here
-	return 42
+	return res
 }
